864ac1af36821c5f8b6331eef445379ac4e9c79f
saaya0 <sayaaa04155@gmail.com>
2022-03-20 17:16:54 +0000

    fix readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :diaries
-  resources :genres
+  resources :genres, only: [:new, :create, :destroy]
   root to: 'diaries#index'
 
 end
